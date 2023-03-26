936fb7d0b4e1e309862bc081048758021ac71033
Robert Rizzo <robtrizzo@gmail.com>
2023-03-26 12:25:58 -0400

    font sizing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -53,7 +53,7 @@ const PostView = (props: PostWithUser) => {
             post.createdAt
           ).fromNow()}`}</span>
         </div>
-        <span>{post.content}</span>
+        <span className="text-xl">{post.content}</span>
       </div>
     </div>
   );
